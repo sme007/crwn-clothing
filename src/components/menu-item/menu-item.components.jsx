03753c9699d7598e3723d63b1bd0e600e615fa1c
Solomon Mashat Emmanuel <semashat@gmail.com>
2020-02-24 15:23:01 +0100

    Updating our app with a shop page, collection preview component and collection item components

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`} >
+const MenuItem = ({title, imageUrl, size, history, liknUrl, match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${liknUrl}`)} >
         <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
     }}  />
@@ -14,6 +15,6 @@ const MenuItem = ({title, imageUrl, size}) => (
 </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
 
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,195 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'hats',
+        routeName: 'hats',
+        items: [
+            {
+                id: 1,
+                name: 'Blue Beanie',
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+                },
+            {
+            id: 2,
+            name: 'Blue Beanie',
+            imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+            price: 18
+            },
+            {
+            id: 3,
+            name: 'Brown Cowboy',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 4,
+            name: 'Black and White longsleeves',
+            imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+            price: 25
+            },
+            {
+            id: 5,
+            name: 'Pink T-shirt',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            {
+            id: 6,
+            name: 'Brown Cowboy',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 7,
+            name: 'Pink T-shirt',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'hats',
+        items: [
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                price: 35
+                },
+            {
+            id: 2,
+            name: 'Adidas Yeezy',
+            imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+            price: 18
+            },
+            {
+            id: 3,
+            name: 'Black Converse',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 4,
+            name: 'Nike White Airforce',
+            imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+            price: 25
+            },
+            {
+            id: 5,
+            name: 'Nike Red High Tops',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            {
+            id: 6,
+            name: 'Nike Brown High Tops',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 7,
+            name: 'Air Jordan Limited',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            {
+            id: 7,
+            name: 'Timberlands',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            
+        ]
+    },
+    
+    {
+        id: 3,
+        title: 'jackets',
+        routeName: 'hats',
+        items: [
+            {
+                id: 4,
+                name: 'Nike White Airforce',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+                },
+            {
+            id: 2,
+            name: 'Blue Jean Jacket',
+            imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+            price: 18
+            },
+            {
+            id: 3,
+            name: 'Grey Jean Jacket',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 4,
+            name: 'Brown Shearling',
+            imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+            price: 25
+            },
+            {
+            id: 5,
+            name: 'Tan Trench',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+                        
+        ]
+    },
+    {
+        id: 4,
+        title: 'womens',
+        routeName: 'hats',
+        items: [
+            {
+                id: 4,
+                name: 'Nike White Airforce',
+                imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+                price: 25
+                },
+            {
+            id: 2,
+            name: 'Floral Blouse',
+            imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+            price: 18
+            },
+            {
+            id: 3,
+            name: 'Floral Dress',
+            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+            price: 35
+            },
+            {
+            id: 4,
+            name: 'Red Dots Dress',
+            imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+            price: 25
+            },
+            {
+            id: 5,
+            name: 'Striped Sweater',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+            {
+            id: 5,
+            name: 'Yellow Track Suit',
+            imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+            price: 25
+            },
+                        
+        ]
+    },
+
+
+]      
+
+export default SHOP_DATA;
