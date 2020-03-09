e9922142d0d1a93f7e02a74ca1a7044b1de294d2
Solomon Mashat Emmanuel <semashat@gmail.com>
2020-03-09 13:08:06 +0100

    Commiting header design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 
 
 function App() {
   return (
-    <div >
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-
-      </Switch>
-      
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />;
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
